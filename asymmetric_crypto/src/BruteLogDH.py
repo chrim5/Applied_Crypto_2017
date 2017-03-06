c0449dbdb30807161bfa932715ff30f8b4c388d0
Marc Christen <marc.christen@students.bfh.ch>
2017-03-06 19:04:53 +0100

    Added Diffie-Hellmann brute force script

---
+++ asymmetric_crypto/src/BruteLogDH.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+import sys
+
+"""
+Goal: compute powers a,b (discrete log)
+by brute force
+Example parameter
+A = 7
+B = 5
+p = 11
+g = 6
+5**3 % 13 = 3
+"""
+
+
+class BruteLogDH:
+    def compute(self, A, B, p, g, range):
+        a = None
+        b = None
+        # Compute powers a,b (discrete log)
+        for i in xrange(range):
+            a = g ** i % p
+            if (A == a):
+                a = i
+                print "a: %s" % (i)
+                break
+        for i in xrange(range):
+            b = g ** i % p
+            if (B == b):
+                b = i
+                print "b: %s" % (i)
+                break
+        # Test DH key
+        kb = B ** a % p
+        ka = A ** b % p
+        if (ka == kb): print "Key valid, k: %s" % (ka)
+
+    if __name__ == '__main__':
+        if len(sys.argv) == 5:
+            A = sys.argv[1]
+            B = sys.argv[2]
+            p = sys.argv[3]
+            g = sys.argv[4]
+            range = sys.argv[5]
+        else:
+            print('Please provide all arguments for A,B,p,g,range')
+            sys.exit(1)
+
+        # Example bruteLog(7, 5, 11, 6, 10)
+        compute(A, B, p, g, range)
---
+++ asymmetric_crypto/src/test_bruteLogDH.py
@@ -0,0 +1,3 @@
+from BruteLogDH import BruteLogDH
+
+BruteLogDH().compute(7, 5, 11, 6, 10)
